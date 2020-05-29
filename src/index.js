e4e682c670a93afdfd7fd455b963a4ba62b07a8a
github username <github email address>
2020-05-29 09:46:02 -0400

    Removed stylesheet href and modified script link in index.html, added event listner for loading the DOM, created const variable for fetch end point, created function for get fetch for all activities from the index controller action

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("Hello you are in index!!!")
+const endFetchPoint = "http://localhost:3000/api/v1/activities"
+
+document.addEventListener('DOMContentLoaded', () => {
+    getActivities()
+})
+
+function getActivities() {
+    fetch(endFetchPoint)
+    .then(response => response.json())
+    .then(activities => {
+        console.log(activities);
+    })
+        
+    }
