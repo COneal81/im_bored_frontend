2ac943fc17d414738458f0e2e34b9ed81f25c5b1
github username <github email address>
2020-05-29 11:24:22 -0400

    added div id activities_container to body of index.html, added variable allActivities and rendered the data to the page using query selector and innerHtml

--- src/index.js
+++ src/index.js
@@ -8,7 +8,21 @@ function getActivities() {
     fetch(endFetchPoint)
     .then(response => response.json())
     .then(activities => {
-        console.log(activities);
+        activities.data.forEach(activities => {
+            // debugger
+            const allActivities = `
+            <div data-id= ${activities.id}>
+            <h2>${activities.attributes.title}</h2>
+            <p>${activities.attributes.description}</p>
+            <p>Category: ${activities.attributes.category.category_name}</p>
+            <button data-id=${activities.id}> Edit </button>
+            </div>
+            <br><br>`
+
+            document.querySelector("#activities_container").innerHTML +=allActivities
+        })
     })
         
     }
+
+    
