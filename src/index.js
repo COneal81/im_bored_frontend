6d4d8a13bdd9422f9476a32dc35354ca5831842a
github username <github email address>
2020-05-29 14:46:46 -0400

    Created event listner and function for form handling in index, created form for adding a new activity in index.html

--- src/index.js
+++ src/index.js
@@ -2,14 +2,24 @@ const endFetchPoint = "http://localhost:3000/api/v1/activities"
 
 document.addEventListener('DOMContentLoaded', () => {
     getActivities()
+
+    const createActivityForm = document.querySelector('#create-activity-form')
+
+    createActivityForm.addEventListener("submit", (e) =>
+    createFormHandler(e))
 })
+
+// This from handler is what grabs all of that value from the submitting of a form.
+
+
+
 
 function getActivities() {
     fetch(endFetchPoint)
     .then(response => response.json())
     .then(activities => {
         activities.data.forEach(activities => {
-            // debugger
+    
             const allActivities = `
             <div data-id= ${activities.id}>
             <h2>${activities.attributes.title}</h2>
@@ -23,6 +33,17 @@ function getActivities() {
         })
     })
         
+    function createFormHandler(e) {
+        e.preventDefault()
+        const titleInput = document.querySelector("#input-title").value
+        const descriptionInput = document.querySelector("#input-description").value
+        const categoryInput = parseInt(document.querySelector("#categories").value)
+        postFetch(titleInput, descriptionInput, categoryInput)
     }
+    
+    function postFetch(titleInput, descriptionInput, category_id) {
+        console.log(titleInput, descriptionInput, category_id)
+    }
+}
 
     
