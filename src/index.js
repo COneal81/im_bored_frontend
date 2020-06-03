69525f0c769fb1bb484e8ba26e2b353fa689d21a
github username <github email address>
2020-06-03 12:36:50 -0400

    Added event listner for edit button in index, added static method findById in activities, created updatedActivityFormHandler function, create patchFetchActivity function, added div update-activity in index.html

--- src/index.js
+++ src/index.js
@@ -1,13 +1,50 @@
 const endFetchPoint = "http://localhost:3000/api/v1/activities"
 
 document.addEventListener('DOMContentLoaded', () => {
+      // fetch and load Activities
     getActivities()
-
+    // listen for a submit event from the form and handle the data beig passed in
     const createActivityForm = document.querySelector('#create-activity-form')
-
     createActivityForm.addEventListener("submit", (e) => createFormHandler(e))
+
+    const editButtonActivitiesContainer = document.querySelector('#activities_container')
+    editButtonActivitiesContainer.addEventListener('click', e => {
+        const id = parseInt(e.target.dataset.id);
+        //  debugger
+        const activity = Activity.findById(id);
+        // debugger
+       
+        document.querySelector("#update-activity").innerHTML = activity.renderActivityUpdateForm();
+    });
+     document.querySelector('#update-activity').addEventListener('submit', e => updateActivityFormHandler(e))
 })
 
+function updateActivityFormHandler(e) {
+    e.preventDefault();
+   
+    const id = parseInt(e.target.dataset.id);
+    const activity = Activity.findById(id);
+    const title = e.target.querySelector("#input-title").value
+    const description= e.target.querySelector("#input-description").value
+    const category_id = parseInt(e.target.querySelector("#categories").value)
+    //  debugger
+    patchFetchActivity(activity, title, description, category_id)
+}
+
+function patchFetchActivity(activity, title, description, category_id){
+    const bodyJSONData = {title, description, category_id}
+    fetch(`http://localhost:3000/api/v1/activities/${activity.id}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify(bodyJSONData),
+    })
+    .then(response => response.json())
+    .then(updatedActivity => console.log(updatedActivity))
+    
+}
 // This from handler is what grabs all of that value from the submitting of a form.
 
 function getActivities() {
@@ -15,8 +52,8 @@ function getActivities() {
     .then(response => response.json())
     .then(activities => {
         activities.data.forEach(activities => {
-        //    debugger
-            let newActivity = new Activity(activities, activities.attributes)
+            // debugger
+            const newActivity = new Activity(activities.id, activities.attributes)
 
             document.querySelector("#activities_container").innerHTML += newActivity.renderActivities()
         })
@@ -43,12 +80,17 @@ function postFetchActivity(title, description, category_id) {
         })
         .then(response => response.json())
         .then(activity => {
-            const activityData = activity.data
-            let newActivity = new Activity(activityData, activityData.attributes)
+            //  const activityData = activity.data
+            const newActivity = new Activity(activity.data.id, activity.data.attributes)
 
             document.querySelector("#activities_container").innerHTML += newActivity.renderActivities()
           
     })
+    
+
+    
+    
+
     
 }
 
